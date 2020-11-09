ea5598016e211b5ba5794c0bcf6a3af99595d3ac
Valuriel <christian.poetter@hotmail.de>
2020-11-09 13:11:18 +0200

    added 4 task

---
+++ 4_task/src/Components/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Article from "../Article/Article";
+import Form from "../Form/Form";
+
+const Main = () => {
+  return (
+    <main>
+      <Form />
+    </main>
+  );
+};
+
+export default Main;
